9047100e03bf5dd0862f07c2e5b294c0d7f90d12
Avinash Malik <avinash.malik@auckland.ac.nz>
2014-02-14 14:26:38 +1300

    Updated make file

--- Tests/parse_metis_file.ml
+++ Tests/parse_metis_file.ml
@@ -32,14 +32,14 @@ let adjwgt = Array.map Nativeint.of_int adjwgt in
 let vsize = Array.map Nativeint.of_int vsize in
 let vwgt = Array.map Nativeint.of_int vwgt in
 
-let (objval,part) = Metis.metis_PartGraphKWay nvtxs ncon xadj adjncy vwgt vsize adjwgt (Nativeint.of_int 2) [||] [||] in
+(* Lets try doing some real partitoning now!! *)
+let (objval,part) = Metis.metis_PartGraphRecursive nvtxs ncon xadj adjncy vwgt vsize adjwgt (Nativeint.of_int 2) [||] [||] in
 let () = print_endline (" Objective value: " ^ (Nativeint.to_string objval)) in
 for i = 0 to Array.length part - 1 do
   print_endline ("(i:part) = " ^ (string_of_int (i+1)) ^ " : " ^ (Nativeint.to_string part.(i)))
 done;
 
-(* Lets try doing some real partitoning now!! *)
-let (objval,part) = Metis.metis_PartGraphRecursive nvtxs ncon xadj adjncy vwgt vsize adjwgt (Nativeint.of_int 2) [||] [||] in
+let (objval,part) = Metis.metis_PartGraphKWay nvtxs ncon xadj adjncy vwgt vsize adjwgt (Nativeint.of_int 2) [||] [||] in
 let () = print_endline (" Objective value: " ^ (Nativeint.to_string objval)) in
 for i = 0 to Array.length part - 1 do
   print_endline ("(i:part) = " ^ (string_of_int (i+1)) ^ " : " ^ (Nativeint.to_string part.(i)))
